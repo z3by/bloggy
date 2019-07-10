9a701b8619ce2e084e1b0971e495e261dd0b1ac5
Ahmad <ahmad.mostafa@britecore.com>
2019-07-10 18:10:28 +0300

    FEAT change admin theme

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include ,path
 
 urlpatterns = [
+    path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
 ]
